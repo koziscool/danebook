40a87e2ec678ab917e750624bd1a7bfe10f67f7f
koziscool <kozjob@yahoo.com>
2016-02-19 16:50:40 -0600

    more user controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -19,4 +19,38 @@ describe UsersController do
     end
 
   end
+
+  describe "Logged In User" do
+
+    let(:user) { create(:user) }
+    let(:user_2) { create(:user) }
+
+    before :each do
+      request.cookies["auth_token"] = user.auth_token
+    end
+
+    it "can GET request to #edit" do
+      #first hash is params hash, second options hash is sessions hash
+      get :edit, { id: user.id }, { user_id: user.id }
+      expect(response).to render_template( :edit )
+    end
+
+    it "tries to GET request another user's #edit but cannot" do
+      user_2
+      get :edit, { id: user_2.id }, { user_id: user.id }
+      expect(response).to redirect_to( root_path )
+    end
+
+    it "can PATCH request to #update" do
+      patch :update, { id: user.id, user: { name: "ASDF" } }, { user_id: user.id }
+      expect(response).to redirect_to redirect_to user_path( assigns(:user) )
+    end
+  end
+  
 end
+
+
+
+
+
+
