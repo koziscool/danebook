c10559cd522ceb0d0a09352c80e571aea845a528
koziscool <kozjob@yahoo.com>
2016-02-24 17:43:15 -0600

    turn on welcome email

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,4 +3,14 @@ module UsersHelper
   def has_already_friended( user1, user2 )
     user1.friends_initiated.exists?( user2 )
   end
+
+  def photo_image_tag( user )
+    if user.avatar 
+       "<%= image_tag @user.avatar.photo_info.url(:medium) %>"
+    else
+        "<img id=\"profile-pic\"  src=\"/assets/harry_potter_small.jpg\" class=\"navbar-form\" alt=\"Responsive Cover\">"
+    end
+
+
+  end 
 end
